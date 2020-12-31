45f87b155edc751df9f66de6dbfaee19473f156e
unknown <luchao01@zybank.com.cn>
2020-12-31 14:51:21 +0800

    初始化工程文件

---
+++ main/main.js
@@ -0,0 +1,14 @@
+class Student{
+    constructor(name,age){
+        this.name=name
+        this.age=age
+    }
+    getName(){
+        return this.name
+    }
+    getAge(){
+        return this.age
+    }
+}
+export default Student
+// module.exports = Student;
